38f31d3b04eb4463e0693040bbd66b9d66b9f64e
Nurmurat-cs1905 <57893017+Nurmurat-cs1905@users.noreply.github.com>
2019-11-21 09:02:42 +0600

    Add files via upload

--- task 98.cpp
+++ task 98.cpp
@@ -1,15 +1,50 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>             //a
+#include <cmath>
 using namespace std;
-    int main (){
-    	int n[10000];
-    	for(int i=1000;i<=9999;i++){
-    		if(i !='5'&& i!='6'){
-    			cout<<i<<" ";
-			}	
-			}
-	
-		
-		
-    	return 0;
-	}
+int main () {
+    double d = 10;
+    for(int i = 2; i <= 10; i++) {
+        d = d + d * 10/100;
+        cout << d << endl;
+    }
+    return 0;
+}
+
+#include <iostream>             //b
+using namespace std;
+int main () {
+    double d = 10, sum = 0;
+    for(int i = 1; i <= 7; i++) {
+        sum += d;
+        d = d + d * 10/100;
+    }
+    cout << sum;
+    return 0;
+}
+
+#include <iostream>            //c
+using namespace std;
+int main () {
+    double d = 10, sum = 0;
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        sum += d;
+        d = d + d * 10/100;
+    }
+    cout << sum;
+    return 0;
+}
+
+#include <iostream>             //d
+using namespace std;
+int main () {
+    double d = 10;
+    int days = 0;
+    while (d <= 80) {
+        d = d + d * 10/100;
+        days++;
+    }
+    cout << days + 1;
+    return 0;
+}
